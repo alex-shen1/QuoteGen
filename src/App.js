ff3c782cf04812681b1a74b8baabcededeedee56
Alex Shen <shen.alexander@gmail.com>
2020-04-18 16:29:16 -0400

    Made app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {random} from 'mathjs';
+import quotesList from "./Text.js"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(props){
+    super(props);
+
+
+    console.log(quotesList);
+    this.state = {
+      activeQuote: "Click the atom to get a new quote!",
+    }
+    // this.myFunc = this.myFunc.bind(this);
+  }
+
+  setQuote = () => {
+    let i = Math.round(random() * 102)
+    this.setState(prevState => {
+      return {
+        activeQuote: quotesList[i] // TODO 
+      }
+    })
+  }
+
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h3>Inspirational Quotes</h3>
+          <br/> <br/>
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          {this.myFunc}
+          {/* {console.log("hi")} */}
+
+
+          <button onClick={()=>{
+            console.log("hi");
+            this.setQuote();
+            }}>
+            <img src={logo} className = "App-logo" />
+          </button>
+
+
+
+          {/* <input type="image" src={logo} className="App-logo" onClick={() => console.log("hi")}/> */}
+          
+          
+          <p>
+            {this.state.activeQuote}
+          </p>
+          
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
